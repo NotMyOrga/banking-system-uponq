28d162ed059d41b3d9a7ecc7d6c76aa8fbd6dd2b
uponq <uponq@student.kit.edu>
2025-06-29 23:36:45 +0200

    Update OnlineShop.java

--- src/main/java/shop/OnlineShop.java
+++ src/main/java/shop/OnlineShop.java
@@ -28,6 +28,10 @@ public class OnlineShop {
 
     public void addToCart(Person buyer, ShopItem item){
         // TODO: Add the item to the buyer's cart. If the buyer doesn't have a cart yet, create one.
+        if (!shoppingCarts.containsKey(buyer)) {
+            shoppingCarts.put(buyer, List.of());
+        }
+        shoppingCarts.get(buyer).add(item);
     }
 
     public boolean placeOrder(Person buyer) {
@@ -37,6 +41,17 @@ public class OnlineShop {
         // 3. Use the bank to transfer the total amount from the buyer to the shop owner.
         // 4. If successful, store the sold items for potential refund and clear the buyer's cart.
         // 5. Return true if purchase succeeded, false otherwise.
+        if (!shoppingCarts.containsKey(buyer)) {
+            return false;
+        }
+        double sum = 0;
+        for (ShopItem item : shoppingCarts.get(buyer)) {
+            sum += item.getPrice();
+        }
+        if (bank.transfer(buyer, shopOwner, sum)) {
+            shoppingCarts.get(buyer).clear();
+            return true;
+        }
         return false;
     }
 }
